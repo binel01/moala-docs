f94271fe9738a51731bdf1558e6a2240a1191e09
BINELI Herve <NRBG1313@adcm.orangecm>
2025-02-21 09:03:47 +0100

    Configuration du déploiement Github Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,15 +17,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://binel01.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'moala', // Usually your GitHub org/user name.
-  projectName: 'moala-api-docs', // Usually your repo name.
+  organizationName: 'binel01', // Usually your GitHub org/user name.
+  projectName: 'moala-docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
